85b80e216b8fa5410cb402882d796fc3ac4bfa01
Arrow <185542076@qq.com>
2018-10-23 13:56:16 +0800

    fix

--- tanblog/settings.py
+++ tanblog/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tanblog',
         'USER': 'root',
-        'PASSWORD': 'jxkj',
+        'PASSWORD': 'jxkj123',
         'HOST': '127.0.0.1',
         'PORT': 3306,
     }
