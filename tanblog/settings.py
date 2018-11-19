c07d4d39022a78dc55747f39e0ecbf8003a20304
Arrow <185542076@qq.com>
2018-11-19 18:17:43 +0800

    移动

--- tanblog/settings.py
+++ tanblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7cmpoq)%mj#k)8c$0-=w@_m*4a(*^hd#7e8gsoro2=t0ue*!_l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '10.8.0.76']
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'haystack',
+    'haystack',                         # haystack全文检索的框架
     'blog',
     'comments',
 ]
