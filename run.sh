5d5466b1b20a0701817fcf41ec2e40663b9ad027
lix16796 <liuxuan2@noahgroup.com>
2021-06-02 19:16:58 +0800

    docker镜像打包运行成功（单独运行镜像）

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# nginx -c /etc/nginx/opssite.conf -g 'daemon off;'
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+gunicorn tanblog.wsgi:application -w 2 -k gthread -b 0.0.0.0:8080 --chdir=/app
